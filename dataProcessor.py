66258c46880519d4c5c61be93ff9835c26792bcd
Sonalika_Singh <144704508+Sonalikasingh17@users.noreply.github.com>
2025-05-10 19:46:34 +0530

    Update dataProcessor.py

--- dataProcessor.py
+++ dataProcessor.py
@@ -1,1 +1,117 @@
+import os
+import random
+import time
+import wandb
+import re, string
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+from wordcloud import WordCloud, STOPWORDS
+from collections import Counter
+from colour import Color
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 
+import tensorflow as tf
+from tensorflow.keras import layers
+import tensorflow.keras.backend as K
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+
+
+
+## Download the dataset ##
+import requests
+import tarfile
+
+def download_data(save_path):
+
+    data_url = r"https://storage.googleapis.com/gresearch/dakshina/dakshina_dataset_v1.0.tar"
+
+    r = requests.get(data_url, allow_redirects=True)
+    tar_path = "data_assignment3.tar"
+
+    if r.status_code == 200:
+        with open(tar_path, 'wb') as f:
+            f.write(r.content)
+
+    tar_file = tarfile.open(tar_path)
+    tar_file.extractall(save_path)
+    tar_file.close()
+
+
+# downloading and extracting the data to drive 
+# comment out the below line if running the cell again
+# download_data("/content/drive/MyDrive/DakshinaDataset")
+
+
+
+
+# Files with English to Devanagari (Hindi) translation word by word 
+# Punctutations have already been cleaned from this file 
+
+def get_data_files(language):
+    """ Function fo read data 
+    """
+
+    ## REPLACE THIS PATH UPTO dakshina_dataset_v1.0 with your own dataset path ##
+    template = "/content/drive/MyDrive/DakshinaDataset/dakshina_dataset_v1.0/{}/lexicons/{}.translit.sampled.{}.tsv"
+
+    train_tsv = template.format(language, language, "train")
+    val_tsv = template.format(language, language, "dev")
+    test_tsv = template.format(language, language, "test")
+
+    return train_tsv, val_tsv, test_tsv
+
+## Utility functions for preprocessing data ##
+
+def add_start_end_tokens(df, cols, sos="\t", eos="\n"):
+    """ Adds EOS and SOS tokens to data 
+    """
+    def add_tokens(s):  
+        # \t = starting token
+        # \n = ending token
+        return sos + str(s) + eos
+
+    for col in cols:
+        df[col] = df[col].apply(add_tokens) 
+    
+def tokenize(lang, tokenizer=None):
+    """ Uses tf.keras tokenizer to tokenize the data/words into characters
+    """
+
+    if tokenizer is None:
+        tokenizer = Tokenizer(char_level=True)
+        tokenizer.fit_on_texts(lang)
+
+        lang_tensor = tokenizer.texts_to_sequences(lang)
+        lang_tensor = tf.keras.preprocessing.sequence.pad_sequences(lang_tensor,
+                                                            padding='post')
+
+    else: 
+        lang_tensor = tokenizer.texts_to_sequences(lang)
+        lang_tensor = tf.keras.preprocessing.sequence.pad_sequences(lang_tensor,
+                                                            padding='post')
+
+    return lang_tensor, tokenizer
+
+def preprocess_data(fpath, input_lang_tokenizer=None, targ_lang_tokenizer=None):
+    """ Reads, tokenizes and adds SOS/EOS tokens to data based on above functions
+    """
+
+    df = pd.read_csv(fpath, sep="\t", header=None)
+
+    # adding start and end tokens to know when to stop predicting 
+    add_start_end_tokens(df, [0,1])
+    
+    input_lang_tensor, input_tokenizer = tokenize(df[1].astype(str).tolist(), 
+                                                    tokenizer=input_lang_tokenizer)
+    
+    targ_lang_tensor, targ_tokenizer = tokenize(df[0].astype(str).tolist(),
+                                                    tokenizer=targ_lang_tokenizer) 
+    
+    dataset = tf.data.Dataset.from_tensor_slices((input_lang_tensor, targ_lang_tensor))
+    dataset = dataset.shuffle(len(dataset))
+    
+    return dataset, input_tokenizer, targ_tokenizer
