83ad41aa6d8f2d10c16088eef721f2dc3b31336a
Sonalika_Singh <144704508+Sonalikasingh17@users.noreply.github.com>
2025-05-14 00:59:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def download_data(save_path):
 
 
 # downloading and extracting the data to drive
-# comment out the below line if running the cell again
+# uncomment the line below if downloading data for the 1st time
 #download_data("/content/drive/MyDrive/DakshinaDataset")
 
 
@@ -286,6 +286,131 @@ class Decoder(tf.keras.Model):
             return output, [state_h], attention_weights
 
 
+class BeamSearch():
+    def __init__(self, model, k):
+        self.k = k
+        self.model = model
+        self.acc = tf.keras.metrics.Accuracy()
+
+    def sample_beam_search(self, probs):
+
+        m, n = probs.shape
+        output_sequences = [[[], 0.0]]
+
+        for row in probs:
+            beams = []
+
+            for tup in output_sequences:
+                seq, score = tup
+                for j in range(n):
+                    new_beam = [seq + [j], score - tf.math.log(row[j])]
+                    beams.append(new_beam)
+
+            output_sequences = sorted(beams, key=lambda x: x[1])[:self.k]
+
+        tensors, scores = list(zip(*output_sequences))
+        tensors = list(map(lambda x: tf.expand_dims(tf.constant(x),0), tensors))
+
+        return tf.concat(tensors, 0), scores
+
+    def beam_accuracy(self, input, target):
+        accs = []
+
+        for i in range(self.k):
+            self.acc.reset_state()
+            self.acc.update_state(target, input[i, :])
+            accs.append(self.acc.result())
+
+        return max(accs)
+
+    def step(self, input, target, enc_state):
+
+        batch_acc = 0
+        sequences = []
+
+        enc_out, enc_state = self.model.encoder(input, enc_state)
+
+        dec_state = enc_state
+        dec_input = tf.expand_dims([self.model.targ_tokenizer.word_index["\t"]]*self.model.batch_size ,1)
+
+        for t in range(1, target.shape[1]):
+
+            preds, dec_state, _ = self.model.decoder(dec_input, dec_state, enc_out)
+
+            sequences.append(preds)
+            preds = tf.argmax(preds, 1)
+            dec_input = tf.expand_dims(preds, 1)
+
+        sequences = tf.concat(list(map(lambda x: tf.expand_dims(x, 1), sequences)), axis=1)
+
+        for i in range(target.shape[0]):
+
+            possibilities, scores = self.sample_beam_search(sequences[i, :, :])
+            batch_acc += self.beam_accuracy(possibilities, target[i, 1:])
+
+        batch_acc = batch_acc / target.shape[0]
+
+        return 0, batch_acc
+
+    def evaluate(self, test_dataset, batch_size=None, upto=5, use_wandb=False):
+
+        if batch_size is not None:
+            self.model.batch_size = batch_size
+            test_dataset = test_dataset.batch(batch_size)
+        else:
+            self.model.batch_size = 1
+
+        test_acc = 0
+        enc_state = self.model.encoder.initialize_hidden_state(self.model.batch_size)
+
+        for batch, (input, target) in enumerate(test_dataset.take(upto)):
+
+           _, acc = self.step(input, target, enc_state)
+           test_acc += acc
+
+        if use_wandb:
+            wandb.log({"test acc (beam search)": test_acc / upto})
+
+        print(f"Test Accuracy on {upto*batch_size} samples: {test_acc / upto:.4f}\n")
+
+    def translate(self, word):
+
+        word = "\t" + word + "\n"
+        sequences = []
+        result = []
+
+        inputs = self.model.input_tokenizer.texts_to_sequences([word])
+        inputs = tf.keras.preprocessing.sequence.pad_sequences(inputs,
+                                                               maxlen=self.model.max_input_len,
+                                                               padding="post")
+
+
+        enc_state = self.model.encoder.initialize_hidden_state(1)
+        enc_out, enc_state = self.model.encoder(inputs, enc_state)
+
+        dec_state = enc_state
+        dec_input = tf.expand_dims([self.model.targ_tokenizer.word_index["\t"]]*1, 1)
+
+        for t in range(1, self.model.max_target_len):
+
+            preds, dec_state, _ = self.model.decoder(dec_input, dec_state, enc_out)
+
+            sequences.append(preds)
+            preds = tf.argmax(preds, 1)
+            dec_input = tf.expand_dims(preds, 1)
+
+        sequences = tf.concat(list(map(lambda x: tf.expand_dims(x, 1), sequences)), axis=1)
+
+        possibilities, scores = self.sample_beam_search(tf.squeeze(sequences, 0))
+        output_words = self.model.targ_tokenizer.sequences_to_texts(possibilities.numpy())
+
+        def post_process(word):
+            word = word.split(" ")[:-1]
+            return "".join([x for x in word])
+
+        output_words = list(map(post_process, output_words))
+
+        return output_words, scores
 
 class Seq2SeqModel():
     def __init__(self, embedding_dim, encoder_layers, decoder_layers, layer_type, units, dropout, attention=False):
@@ -875,7 +1000,8 @@ def train_with_wandb(language, test_beam_search=False):
   
     #wandb.run.name 
     wandb.run.name=f"edim_{wandb.config.embedding_dim}_edl_{wandb.config.enc_dec_layers}_lr_{wandb.config.layer_type}_units_{wandb.config.units}_dp_{wandb.config.dropout}_att_{wandb.config.attention}_bw_{wandb.config.beam_width}_tfc_{wandb.config.teacher_forcing_ratio}"
-
+    # Change run name for the sweep with attention
+	#"layers_{wandb.config.enc_dec_layers}_units_{wandb.config.units}_dp_{wandb.config.dropout}_att_{wandb.config.attention}"
 
     ## 1. SELECT LANGUAGE ##
     TRAIN_TSV, VAL_TSV, TEST_TSV = get_data_files(language)
@@ -900,7 +1026,7 @@ def train_with_wandb(language, test_beam_search=False):
                 metric = tf.keras.metrics.SparseCategoricalAccuracy())
 
     ## 5. FITTING AND VALIDATING THE MODEL
-    model.fit(dataset, val_dataset, epochs=5, use_wandb=True, teacher_forcing_ratio=wandb.config.teacher_forcing_ratio)
+    model.fit(dataset, val_dataset, epochs=10, use_wandb=True, teacher_forcing_ratio=wandb.config.teacher_forcing_ratio)
 
     if test_beam_search:
         ## OPTIONAL :- Evaluate the dataset using beam search and without beam search
@@ -950,9 +1076,10 @@ sweep_config = {
 }
 
 #sweep_id = wandb.sweep(sweep_config, project="DA6401-Assignment_03")
-wandb.agent(sweep_id,function=lambda: train_with_wandb("hi"),project="DA6401-Assignment_03" )
+wandb.agent(sweep_id,function=lambda: train_with_wandb("hi"),project="DA6401-Assignment_03", count = 20)
 
 # Sweep function with attention mechanism
+# Run this hyperparameter sweep for attention mechanism
 '''
 sweep_config2 = {
   "name": "Attention Sweep - Assignment3",
@@ -975,7 +1102,8 @@ sweep_config2 = {
 }
 
 sweep_id2 = wandb.sweep(sweep_config2, project="DA6401-Assignment_03")
-wandb.agent(sweep_id2,function=lambda: train_with_wandb("hi", test_beam_search=True),project="DA6401-Assignment_03" , count = 5)
+wandb.agent(sweep_id2,function=lambda: train_with_wandb("hi", test_beam_search=True),project="DA6401-Assignment_03" , count = 10)
 
 '''
-
+## Change the run name for this attention sweep 
+"layers_{wandb.config.enc_dec_layers}_units_{wandb.config.units}_dp_{wandb.config.dropout}_att_{wandb.config.attention}"
